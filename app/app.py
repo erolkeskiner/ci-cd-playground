e99904b8f847cea78cc8fc57e9d41f20d5ac1eb2
erol.keskiner <erol.keskiner@softtech.com.tr>
2021-01-20 13:25:57 +0300

    gather host and port info from env vars, might consider to use a config.ini file or another configuration

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from os import environ
 
 app = Flask(__name__)
 
@@ -10,3 +11,15 @@ def hello_world():
         return 'Hello {}!'.format(name)
     else:
         return 'Hello Stranger!'
+
+
+if __name__ == '__main__':
+    try:
+        host = environ["FLASK_RUN_HOST"]
+    except KeyError:
+        host = '0.0.0.0'
+    try:
+        port = environ["FLASK_RUN_PORT"]
+    except KeyError:
+        port = '8080'
+    app.run(host=host, port=port)
