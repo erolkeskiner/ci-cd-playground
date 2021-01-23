6a6bc836159e502f1e084afe86d1952a9bee8d3f
erol.keskiner <erol.keskiner@softtech.com.tr>
2021-01-23 15:01:26 +0300

    no need to get host and port info from env vars since they can be set via command line flag

--- app/app.py
+++ app/app.py
@@ -15,12 +15,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    try:
-        host = environ["FLASK_RUN_HOST"]
-    except KeyError:
-        host = '0.0.0.0'
-    try:
-        port = environ["FLASK_RUN_PORT"]
-    except KeyError:
-        port = '8080'
-    app.run(host=host, port=port)
+    app.run()
