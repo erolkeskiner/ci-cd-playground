24964444812d855ffdb7bd9f49b37a840602b8be
erol.keskiner <erol.keskiner@softtech.com.tr>
2021-01-27 00:41:52 +0300

    log http method, the path and the http status code after each request

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -1,10 +1,11 @@
+import logging
 import os
 
 from flask import Flask, request
 from flask_healthz import healthz
+
 from .routes.hello_world import hello_world_blueprint
 from .routes.welcome import welcome_blueprint
-
 
 def create_app():
     app = Flask(__name__)
@@ -19,5 +20,14 @@ def create_app():
         app.config.from_object("config.TestingConfig")
     else:
         app.config.from_object("config.DevelopmentConfig")
+
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+
+    @app.after_request
+    def log_after_request(response):
+        app.logger.info('{} {} {}'.format(request.method, request.full_path, response.status))
+        return response
 
     return app
